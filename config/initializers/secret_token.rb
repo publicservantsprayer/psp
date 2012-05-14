630f7377835ff72564f8f13691d3bb2ab850ba9c
naterumba <naterumba@gmail.com>
2012-05-14 11:01:08 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Psp::Application.config.secret_token = 'ee199077b54c067ff1606384ee3daa3db9678a695140a91e573930ac3a6cbc873edf274ddb7c56e5f7fa0f266fcd9aeacdd89b0e0e6d2b56ddf9258a4dfaa77a'
