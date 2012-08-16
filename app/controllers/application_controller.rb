45f156a81f9b578c3b9d088437cc8140512db736
naterumba <naterumba@gmail.com>
2012-08-16 06:24:24 -0500

    Show different titles on different domains

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :get_ip
+  helper_method :usaprays?
 
   def get_ip
     @client_ip = request.remote_ip
     @client_ip_forwarded = request.env["HTTP_X_FORWARDED_FOR"]
+  end
+
+  def usaprays?
+    request.env["HTTP_HOST"].include? "usaprays.org"
   end
 
 end
