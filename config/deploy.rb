20a2b5fb34e103e36a3782407ac7bfdd7a17851c
nathan amick <github@nathanamick.com>
2012-11-20 00:51:15 +0000

    Add photos back in

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,6 +39,7 @@ namespace :deploy do
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/config/initializers/mail_chimp.rb #{release_path}/config/initializers/mail_chimp.rb"
+    run "ln -nfs #{shared_path}/photos #{release_path}/public/photos"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
